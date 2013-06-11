091fb4094bcf6895ae8da9409ad95916685df078
r02548 <bruger@fullsail.edu>
2013-06-10 22:30:40 -0400

    Beginning my story by setting up my variables.

--- ruger_brandon_assignment2.js
+++ ruger_brandon_assignment2.js
@@ -1,1 +1,26 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Brandon Ruger
+// SDI 1306
+// Project 2
+var myDog = "Ryder"
+var age = 2
+var favoriteToys = [antler, kong, giraffe, nylabone, ball]
+
+// Procedure Function
+
+
+
+// Boolean Function
+
+// Number Function
+
+// String Function
+
+// Array Function
+
+
+
+// Main Code
+console.log("This is a story about my dog named " + myDog + ".");
+
