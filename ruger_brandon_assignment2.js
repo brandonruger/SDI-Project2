b1181ed1be922b7f0efeb8ed0b35856d8261ed9e
r02548 <bruger@fullsail.edu>
2013-06-10 22:41:37 -0400

    Created procedure function.

--- ruger_brandon_assignment2.js
+++ ruger_brandon_assignment2.js
@@ -6,9 +6,15 @@
 var myDog = "Ryder"
 var age = 2
 var favoriteToys = [antler, kong, giraffe, nylabone, ball]
+var store = "All About Puppies"
 
 // Procedure Function
 
+var adoptedDog = function(newDog){
+    console.log("In August 2011 I went to " + store + " where I first met " + myDog + ".");
+    console.log("I fell in love with him immediately, even though I wasn't looking for " + newDog + ".");
+    console.log("I couldn't leave without him, so I decided I was going to take him home.");
+};
 
 
 // Boolean Function
@@ -23,4 +29,5 @@ var favoriteToys = [antler, kong, giraffe, nylabone, ball]
 
 // Main Code
 console.log("This is a story about my dog named " + myDog + ".");
+adoptedDog("a new dog");
 
