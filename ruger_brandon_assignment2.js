361a8b8e38f219d34e5e5258a53656f5db18e5a5
r02548 <bruger@fullsail.edu>
2013-06-10 22:51:15 -0400

    Finished procedure function.

--- ruger_brandon_assignment2.js
+++ ruger_brandon_assignment2.js
@@ -3,17 +3,23 @@
 // Brandon Ruger
 // SDI 1306
 // Project 2
-var myDog = "Ryder"
-var age = 2
-var favoriteToys = [antler, kong, giraffe, nylabone, ball]
-var store = "All About Puppies"
+var myDog = "Ryder";
+var age = 2;
+var favoriteToys = ["antler", "kong", "giraffe", "nylabone", "ball"];
+var store = "All About Puppies";
 
 // Procedure Function
 
 var adoptedDog = function(newDog){
+    var enoughMoney = true;
     console.log("In August 2011 I went to " + store + " where I first met " + myDog + ".");
     console.log("I fell in love with him immediately, even though I wasn't looking for " + newDog + ".");
     console.log("I couldn't leave without him, so I decided I was going to take him home.");
+    console.log("First though, I had to see if I had enough money.");
+    // Argument Conditional
+    if (enoughMoney) {
+        console.log("It's true that I had enough money, so I took him home.");
+    } else console.log("I didn't have enough money, but I used a credit card.");
 };
 
 
